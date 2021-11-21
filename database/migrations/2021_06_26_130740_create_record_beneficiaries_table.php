cd7e1a9e42d5668fd7592843e1505ccf3c269048
Mohamed Fathy <mohammad.soliman44@gmail.com>
2021-11-21 16:34:12 +0200

    office

--- database/migrations/2021_06_26_130740_create_record_beneficiaries_table.php
+++ database/migrations/2021_06_26_130740_create_record_beneficiaries_table.php
@@ -17,7 +17,7 @@ class CreateRecordBeneficiariesTable extends Migration
             $table->id();
             $table->unsignedInteger('individual_id');
             $table->unsignedInteger('record_id');
-            $table->boolean('is_direct');
+            $table->boolean('is_direct')->default(-1);
             $table->timestamps();
 
             // foreign keys
